8cd131b15eb214fbb0d403fd1271c96c10f0504f
garlicOS® <14845347+Grosserly@users.noreply.github.com>
2020-10-19 12:14:28 -0500

    Updates

---
+++ insult.py
@@ -0,0 +1,27 @@
+item1 = [
+  "You",
+  "You're a",
+  "You are a"
+]
+
+item2 = [
+  "damn",
+  "god damn",
+  "fucking",
+  "bumbling",
+  "absolute utter",
+  "utter",
+  "absolute"
+]
+
+item3 = [
+  "dumbass.",
+  "idiot.",
+  "moron.",
+  "asshole.",
+  "fool.",
+  "piece of trash.",
+  "cunt.",
+  "prick.",
+  "clampongus"
+]

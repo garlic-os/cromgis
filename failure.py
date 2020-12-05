b7f922be386b3402017f75c6631947a4129a703d
InvalidOS <35383068+InvalidOS@users.noreply.github.com>
2020-12-05 16:03:20 -0500

    Update and rename failure_phrases.py to failure.py

---
+++ failure.py
@@ -0,0 +1,17 @@
+failure_phrases = [
+    "Wrong.",
+    "You have failed.",
+    "Incorrect.",
+    "no",
+    "You utter fool",
+    "Fool...",
+    "Error!",
+    "ERROR:",
+    "You have erred.",
+    "Imagine writing code that makes this error:",
+    "Do you are have dumb?",
+    "what!",
+    "Bad at computer!",
+    "Bad at computer. Bad at computer!",
+    "oh no"
+]

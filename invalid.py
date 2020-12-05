d3eb93ee9e8d1ffc670c0fe5a13128d7acbc618a
InvalidOS <35383068+InvalidOS@users.noreply.github.com>
2020-12-05 16:48:19 -0500

    fixes

--- invalid.py
+++ invalid.py
@@ -194,8 +194,8 @@ class InvalidCommands(commands.Cog):
   @commands.command(aliases=["asherifyme", "asherme"])
   async def asherizeme(self, ctx: commands.Context):
       """Asherizes/de-asherizes you, making you speak in asherisms."""
-      if not str(ctx.message.author.id) == "286883056793681930":
-        asherizeUser(str(ctx.message.author.id))
+      if not ctx..author.id == 286883056793681930:
+        asherizeUser(str(ctx.author.id))
       else:
         embed = Crombed(
             title = random.choice(failure_phrases),

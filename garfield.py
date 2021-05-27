aa8fa68c44e4c7410e09814f32664e5b476f0fbb
the-garlic-os <14845347+the-garlic-os@users.noreply.github.com>
2021-05-27 01:17:43 -0400

    Add 'yesterday' and 'tomorrow' options; use correct timezone

--- garfield.py
+++ garfield.py
@@ -1,17 +1,15 @@
-from typing import BinaryIO
 
-import os
-from datetime import date, timedelta
-from dateutil.parser import parse as parse_date
 from io import BytesIO
-from dotenv import load_dotenv
+import random
+from datetime import datetime, timedelta, timezone
+from dateutil.parser import parse as parse_date
 from discord.ext import commands
 import aiohttp
 import discord
-import random
 
 GARFIELD_URL = "https://www.gocomics.com/garfield/"
-FIRST_COMIC_DATE = date(year=1978, month=6, day=19)
+GARFIELD_TIMEZONE = timezone(offset=timedelta(hours=-5))
+FIRST_COMIC_DATE = datetime(year=1978, month=6, day=19)
 
 
 class GarfieldCommand(commands.Cog):
@@ -41,28 +39,32 @@ class GarfieldCommand(commands.Cog):
                 return BytesIO(await response.read())
 
 
-    def random_comic_date(self) -> date:
+    def random_comic_date(self) -> datetime:
         """ Random date between today and the first Garfield comic. """
-        days_range = date.today() - FIRST_COMIC_DATE
+        days_range = datetime.now() - FIRST_COMIC_DATE
         random_day_since_first = random.randint(0, days_range.days)
         return FIRST_COMIC_DATE + timedelta(days=random_day_since_first)
 
 
     @commands.command(aliases=["garf", "dailygarfield"])
-    async def garfield(self, ctx: commands.Context, *, comic_date: str=None) -> None:
+    async def garfield(self, ctx: commands.Context, *, comic_date_text: str=None) -> None:
         """
         Download a Garfield comic by date and post it to Discord.
         If no date given, download a random comic.
         Download today's with "today" or "now".
         """
-        comic_date = comic_date.lower()
-        if comic_date is not None:
-            if comic_date in ("today", "now"):
-                comic_date = date.today()
-            elif comic_date == "random":
+        if comic_date_text is not None:
+            comic_date_text = comic_date_text.lower()
+            if comic_date_text in ("today", "now"):
+                comic_date = datetime.now(tz=GARFIELD_TIMEZONE)
+            elif comic_date_text == "yesterday":
+                comic_date = datetime.now(tz=GARFIELD_TIMEZONE) - timedelta(days=1)
+            elif comic_date_text == "tomorrow":
+                comic_date = datetime.now(tz=GARFIELD_TIMEZONE) - timedelta(days=364)
+            elif comic_date_text == "random":
                 comic_date = self.random_comic_date()
             else:
-                comic_date = parse_date(date)
+                comic_date = parse_date(comic_date_text)
         else:
             comic_date = self.random_comic_date()
 

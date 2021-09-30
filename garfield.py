69607e8daef290863863c25b0dadc05056da6efc
the-garlic-os <14845347+the-garlic-os@users.noreply.github.com>
2021-09-30 17:22:22 -0400

    Post comic URL directly

--- garfield.py
+++ garfield.py
@@ -15,6 +15,23 @@ FIRST_COMIC_DATE = datetime(year=1978, month=6, day=19)
 class GarfieldCommand(commands.Cog):
     """ Also made by garlicOS® """
 
+    def parse_comic_date_text(self, comic_date_text: str) -> datetime:
+        if comic_date_text is not None:
+            comic_date_text = comic_date_text.lower()
+            if comic_date_text in ("today", "now"):
+                comic_date = datetime.now(tz=GARFIELD_TIMEZONE)
+            elif comic_date_text == "yesterday":
+                comic_date = datetime.now(tz=GARFIELD_TIMEZONE) - timedelta(days=1)
+            elif comic_date_text == "tomorrow":
+                comic_date = datetime.now(tz=GARFIELD_TIMEZONE) - timedelta(days=364)
+            elif comic_date_text == "random":
+                comic_date = self.random_comic_date()
+            else:
+                comic_date = parse_date(comic_date_text)
+        else:
+            comic_date = self.random_comic_date()
+        return comic_date
+
     async def get_comic_url_by_date(self, year_month_day: str) -> str:
         """ Get the URL of a Garfield comic by date, formatted 'yyyy/mm/dd'. """
         url = GARFIELD_URL + year_month_day
@@ -53,34 +70,15 @@ class GarfieldCommand(commands.Cog):
         If no date given, download a random comic.
         Download today's with "today" or "now".
         """
-        if comic_date_text is not None:
-            comic_date_text = comic_date_text.lower()
-            if comic_date_text in ("today", "now"):
-                comic_date = datetime.now(tz=GARFIELD_TIMEZONE)
-            elif comic_date_text == "yesterday":
-                comic_date = datetime.now(tz=GARFIELD_TIMEZONE) - timedelta(days=1)
-            elif comic_date_text == "tomorrow":
-                comic_date = datetime.now(tz=GARFIELD_TIMEZONE) - timedelta(days=364)
-            elif comic_date_text == "random":
-                comic_date = self.random_comic_date()
-            else:
-                comic_date = parse_date(comic_date_text)
-        else:
-            comic_date = self.random_comic_date()
+        comic_date = self.parse_comic_date_text(comic_date_text)
 
         year_month_day = comic_date.strftime("%Y/%m/%d")
         print(f"Fetching comic URL for {year_month_day}...")
         comic_url = await self.get_comic_url_by_date(year_month_day)
 
         print("Got comic URL:", comic_url)
-        print("Downloading...")
-        comic = await self.load_file_from_url(comic_url)
-
-        print(f"Posting to {ctx.channel}...")
-        await ctx.send(file=discord.File(
-            fp=comic,
-            filename=comic_date.strftime("%Y-%m-%d") + ".gif",
-        ))
+        print(f"Posting to #{ctx.channel}...")
+        await ctx.send(comic_url)
 
 
 def setup(bot):

6640ca11e068fe484873ad97fbbb64abd825783a
Nate <14845347+garlic-os@users.noreply.github.com>
2023-08-23 19:33:32 -0300

    Crombed: Remove unused features

--- utils.py
+++ utils.py
@@ -6,8 +6,8 @@ import string
 class Crombed(discord.Embed):
     """
     A Discord Embed with extra features!
-    Concept definitely not stolen from crimsoBOT's source code:
-    https://github.com/crimsobot/crimsoBOT/blob/0bbe9d0847d169ff5d124c92f00c2e71127d021a/crimsobot/utils/tools.py
+    Concepts stolen from crimsoBOT; copyright (c) 2019 crimso, williammck; MIT
+    https://github.com/crimsobot/crimsoBOT/blob/master/crimsobot/utils/tools.py
     """
     colors = {
         "flesh": 0x98784C,
@@ -22,49 +22,6 @@ class Crombed(discord.Embed):
             kwargs["color"] = Crombed.colors[color_name]
             kwargs.pop("color_name", None)
         super().__init__(**kwargs)
-
-        self.crombed_args = kwargs
-
-        self.set_color_by_name()
-        self.set_author_by_user()
-        self.set_image_by_name()
-
-        del self.crombed_args
-
-
-    def set_color_by_name(self):
-        """ Set the embed's color by color_name if color is not defined. """
-        """ If neither are defined, default to flesh color. """
-        if self.color:  # Let color override color_name
-            return
-
-        colors = {
-            "flesh": 0x98784C,
-            "red":   0xD32F2F,
-            "squid": 0xBEF4C3,
-            "teal":  0x1EC9A1
-        }
-
-        color_name = self.crombed_args.get("color_name", "flesh")
-        self.color = colors[color_name]
-
-
-    def set_author_by_user(self):
-        """ Set the embed's author by a DiscordUser. """
-        author = self.crombed_args.get("author", None)
-
-        if author:
-            name = author.display_name
-            icon_url = author.avatar_url_as(size=32)
-            self.set_author(name=name, icon_url=icon_url)
-
-
-    def set_image_by_name(self):
-        """ Set the embed's image by the URL of the image. """
-        url = self.crombed_args.get("image_url", None)
-
-        if url:
-            self.set_image(url=url)
 
 
 

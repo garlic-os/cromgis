9788a66e77e85aeaf2f3d6498b32aae065324c75
Nate Kean <14845347+garlic-os@users.noreply.github.com>
2025-01-14 13:08:20 -0600

    Memory optimization: have fewer large objects around at once

--- pxl_srt.py
+++ pxl_srt.py
@@ -37,17 +37,22 @@ def sort_pixels_by_color(img: Image.Image) -> Image.Image:
 	solst-ice/pxl-srt Python+PIL port
 	https://github.com/solst-ice/pxl-srt/commit/5e82ae6/src/utils/imageProcessing.js
 	"""
-	# 2D array of image's pixels as received from this function crimsoBOT uses
+	# 2D array of image's pixels
 	# shape: (..., ..., 4)
 	raster: npt.NDArray[np.uint8] = plt.pil_to_array(img)
-	# assert len(raster.shape) == 3
-	# assert raster.shape[2] == 4
+
+	# Keep the raster's shape in a separate variable because it is going to
+	# outlive the rest of the object
+	raster_shape = raster.shape
+
+	# assert len(raster_shape) == 3
+	# assert raster_shape[2] == 4
 
 	# 1D array of raster's pixels + extra information per pixel. So pixels can
 	# be sorted as a 1D array
 	# shape: (..., 6)
 	pixels = np.ndarray(
-		(raster.shape[0] * raster.shape[1]),
+		(raster_shape[0] * raster_shape[1]),
 		dtype=[
 			("r", np.uint8),
 			("g", np.uint8),
@@ -59,8 +64,8 @@ def sort_pixels_by_color(img: Image.Image) -> Image.Image:
 	)
 
 	# Convert pixels to array of objects with color information
-	for i in range(raster.shape[0]):
-		for j in range(raster.shape[1]):
+	for i in range(raster_shape[0]):
+		for j in range(raster_shape[1]):
 			pixel: tuple[np.uint8, np.uint8, np.uint8, np.uint8] = raster[i, j]
 			r, g, b, a = pixel
 
@@ -88,24 +93,27 @@ def sort_pixels_by_color(img: Image.Image) -> Image.Image:
 			else:
 				color_group = ColorGroup.MIXED
 
-			pixels[i * raster.shape[1] + j][0] = r
-			pixels[i * raster.shape[1] + j][1] = g
-			pixels[i * raster.shape[1] + j][2] = b
-			pixels[i * raster.shape[1] + j][3] = a
-			pixels[i * raster.shape[1] + j][4] = color_group.value
-			pixels[i * raster.shape[1] + j][5] = luminosity
+			pixels[i * raster_shape[1] + j][0] = r
+			pixels[i * raster_shape[1] + j][1] = g
+			pixels[i * raster_shape[1] + j][2] = b
+			pixels[i * raster_shape[1] + j][3] = a
+			pixels[i * raster_shape[1] + j][4] = color_group.value
+			pixels[i * raster_shape[1] + j][5] = luminosity
+
+	# Done with the old raster; throw it away and create a new one that we can
+	# write to
+	raster = np.ndarray(shape=raster_shape, dtype=np.uint8)
 
 	# Sort pixels by color group first, then by luminosity
+	# TODO: optimization: use `indices` to copy `pixels` into `raster` directly
 	indices = np.lexsort((pixels["luminosity"], pixels["color_group"]))
-	sorted_pixels = pixels[indices]
+	pixels = pixels[indices]  # the big object in question
 
-	# Put sorted pixels back into the raster
-	# throw away the old raster, create a new one that we can write to
-	raster = np.ndarray(shape=raster.shape, dtype=np.uint8)
-	for i in range(raster.shape[0]):
-		for j in range(raster.shape[1]):
+	# Put sorted pixels back into the raster.
+	for i in range(raster_shape[0]):
+		for j in range(raster_shape[1]):
 			for k in range(4):
-				raster[i, j, k] = sorted_pixels[i * raster.shape[1] + j][k]
+				raster[i, j, k] = pixels[i * raster_shape[1] + j][k]
 
 	sorted_raster_fp = BytesIO()
 	plt.imsave(sorted_raster_fp, raster)

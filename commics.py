74bfc62a924369bdfe687cbde754cad12e5f2723
Nate Kean <14845347+garlic-os@users.noreply.github.com>
2025-01-30 15:25:35 -0600

    commics.py: Fix it

--- commics.py
+++ commics.py
@@ -1,65 +1,69 @@
-import comics
 import datetime as dt
+
+import comics
+import comics.gocomics
 from discord.ext import commands
+
 
 GOCOMICS_TIMEZONE = dt.timezone(offset=dt.timedelta(hours=-5))
 ALIASES = {
-    "calvinandhobbes": ["ch", "cnh", "calvin & hobbes"],
-    "garfield": ["garf"],
+	"calvinandhobbes": ["ch", "cnh", "calvin & hobbes", "calvin and hobbes"],
+	"garfield": ["garf"],
+	"healthcliff": ["hc", "heath"],
 }
 
 
 class Comics(commands.Cog):
-    """Also made by garlicOS®"""
+	"""Also made by garlicOS®"""
 
-    def __init__(self, bot: commands.Bot):
-        self.bot = bot
+	@staticmethod
+	def get_comic_api(
+		name: str, date: str | None | dt.date | dt.datetime
+	) -> comics.gocomics.ComicsAPI:
+		search = comics.search(name)
+		if date is not None:
+			date = date or date.lower()
+		match date:
+			case "random" | None:
+				return search.random_date()
+			case "today" | "now":
+				date = dt.datetime.now(tz=GOCOMICS_TIMEZONE)
+			case "yesterday":
+				date = dt.datetime.now(tz=GOCOMICS_TIMEZONE) - dt.timedelta(
+					days=1
+				)
+			case "tomorrow":
+				now = dt.datetime.now(tz=GOCOMICS_TIMEZONE)
+				date = dt.date(
+					year=now.year - 1, month=now.month, day=now.day - 1
+				)
+		return search.date(date)
 
-    def get_comic_api(
-        self, name: str, date: str | dt.datetime | None
-    ):  # comics.ComicAPI but its not exposed in the module
-        search = comics.search(name)
-        if date is str:
-            date = date.lower()
-        match date:
-            case "random" | None:
-                return search.random_date()
-            case "today" | "now":
-                date = dt.datetime.now(tz=GOCOMICS_TIMEZONE)
-            case "yesterday":
-                date = dt.datetime.now(tz=GOCOMICS_TIMEZONE) - dt.timedelta(days=1)
-            case "tomorrow":
-                date = (
-                    dt.datetime.now(tz=GOCOMICS_TIMEZONE)
-                    - dt.timedelta(years=1)
-                    + dt.timedelta(days=1)
-                )
-        return search.date(date)
+	@staticmethod
+	def parse_aliases(name: str) -> str:
+		name = name.lower()
+		for official_name in ALIASES:
+			if name == official_name or name in ALIASES[official_name]:
+				return official_name
+		try:
+			return comics.directory.search(name)[0]
+		except IndexError:
+			raise commands.BadArgument(f"Comic '{name}' not found")
 
-    def parse_aliases(self, name: str) -> str:
-        name = name.lower()
-        for official_name in ALIASES:
-            if name == official_name or name in ALIASES[official_name]:
-                return official_name
-        try:
-            return comics.directory.search(name)[0]
-        except IndexError:
-            raise commands.BadArgument(f"Comic '{name}' not found")
+	@commands.command()
+	async def comic(
+		self, ctx: commands.Context, name: str, date: str | None = None
+	) -> None:
+		name = Comics.parse_aliases(name)
+		api = Comics.get_comic_api(name, date)
+		await ctx.send(api.image_url)
 
-    @commands.command()
-    async def comic(
-        self, ctx: commands.Context, name: str, date_string: str | None = None
-    ) -> None:
-        name = self.parse_aliases(name)
-        api = self.get_comic_api(name, date_string)
-        await ctx.send(api.image_url)
-
-    @commands.command(aliases=ALIASES["garfield"])
-    async def garfield(
-        self, ctx: commands.Context, date_string: str | None = None
-    ) -> None:
-        await self.comic(ctx, "garfield", date_string)
+	@commands.command(aliases=ALIASES["garfield"])
+	async def garfield(
+		self, ctx: commands.Context, date: str | None = None
+	) -> None:
+		await self.comic(ctx, "garfield", date)
 
 
 async def setup(bot: commands.Bot) -> None:
-    await bot.add_cog(Comics(bot))
+	await bot.add_cog(Comics())

85b2963a079975900b1bcfc9bcbb05ded61182b3
Nate Kean <14845347+garlic-os@users.noreply.github.com>
2025-01-30 16:02:28 -0600

    Be timezone-aware while keeping comics API timezone-naive

--- commics.py
+++ commics.py
@@ -5,7 +5,27 @@ import comics.gocomics
 from discord.ext import commands
 
 
-GOCOMICS_TIMEZONE = dt.timezone(offset=dt.timedelta(hours=-5))
+def timedelta_normalize_timezone() -> dt.timedelta:
+	"""
+	Normalize datetimes instead of making them timezone-aware because the comics
+	module uses timezone-naive datetimes internally
+	"""
+	system_now = dt.datetime.now().astimezone()
+	# If for some reason your timezone's UTC offset can't be identified, just
+	# default it to 0. Not that big of a deal
+	system_utc_offset = dt.datetime.utcoffset(system_now) or dt.timedelta(
+		hours=0
+	)
+	gocomics_timezone = dt.timezone(offset=dt.timedelta(hours=-5))
+	gocomics_now = dt.datetime.now().astimezone(gocomics_timezone)
+	gocomics_utc_offset = dt.datetime.utcoffset(gocomics_now) or dt.timedelta(
+		hours=0
+	)
+	return system_utc_offset - gocomics_utc_offset
+
+
+TIMEDELTA_NORMALIZE_TIMEZONE = timedelta_normalize_timezone()
+
 ALIASES = {
 	"calvinandhobbes": ["ch", "cnh", "calvin & hobbes", "calvin and hobbes"],
 	"garfield": ["garf"],
@@ -17,26 +37,33 @@ class Comics(commands.Cog):
 	"""Also made by garlicOS®"""
 
 	@staticmethod
-	def get_comic_api(
-		name: str, date: str | None | dt.date | dt.datetime
-	) -> comics.gocomics.ComicsAPI:
-		search = comics.search(name)
-		if date is not None:
-			date = date or date.lower()
-		match date:
-			case "random" | None:
-				return search.random_date()
+	def parse_date_string(
+		date_string: str | None,
+	) -> dt.datetime | dt.date | None:
+		if date_string is not None:
+			date_string = date_string.lower()
+		match date_string:
 			case "today" | "now":
-				date = dt.datetime.now(tz=GOCOMICS_TIMEZONE)
+				date = dt.datetime.now()
 			case "yesterday":
-				date = dt.datetime.now(tz=GOCOMICS_TIMEZONE) - dt.timedelta(
-					days=1
-				)
+				date = dt.datetime.now() - dt.timedelta(days=1)
 			case "tomorrow":
-				now = dt.datetime.now(tz=GOCOMICS_TIMEZONE)
+				now = dt.datetime.now()
 				date = dt.date(
 					year=now.year - 1, month=now.month, day=now.day - 1
 				)
+			case _:
+				return None
+		return date + TIMEDELTA_NORMALIZE_TIMEZONE
+
+	@staticmethod
+	def get_comic_api(
+		name: str, date_string: str | None
+	) -> comics.gocomics.ComicsAPI:
+		search = comics.search(name)
+		date = Comics.parse_date_string(date_string)
+		if date is None:
+			return search.random_date()
 		return search.date(date)
 
 	@staticmethod
